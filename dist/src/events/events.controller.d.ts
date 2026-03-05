a2934ef3f2600b6151c4d09eebe024a4f2b1eb54
sovsianykov <serhii.ovsianykov@dataart.com>
2026-03-05 13:20:16 +0200

    added render.yaml one more time 3

--- dist/src/events/events.controller.d.ts
+++ dist/src/events/events.controller.d.ts
@@ -5,52 +5,52 @@ export declare class EventsController {
     private readonly eventsService;
     constructor(eventsService: EventsService);
     create(dto: CreateEventDto): import("@prisma/client").Prisma.Prisma__CalendarEventClient<{
-        id: string;
         title: string;
         date: string;
         time: string | null;
         category: import("@prisma/client").$Enums.EventCategory;
         description: string | null;
+        id: string;
         createdAt: Date;
         updatedAt: Date;
     }, never, import("@prisma/client/runtime/library").DefaultArgs, import("@prisma/client").Prisma.PrismaClientOptions>;
     findAll(date?: string, category?: string): import("@prisma/client").Prisma.PrismaPromise<{
-        id: string;
         title: string;
         date: string;
         time: string | null;
         category: import("@prisma/client").$Enums.EventCategory;
         description: string | null;
+        id: string;
         createdAt: Date;
         updatedAt: Date;
     }[]>;
     findOne(id: string): Promise<{
-        id: string;
         title: string;
         date: string;
         time: string | null;
         category: import("@prisma/client").$Enums.EventCategory;
         description: string | null;
+        id: string;
         createdAt: Date;
         updatedAt: Date;
     }>;
     update(id: string, dto: UpdateEventDto): Promise<{
-        id: string;
         title: string;
         date: string;
         time: string | null;
         category: import("@prisma/client").$Enums.EventCategory;
         description: string | null;
+        id: string;
         createdAt: Date;
         updatedAt: Date;
     }>;
     remove(id: string): Promise<{
-        id: string;
         title: string;
         date: string;
         time: string | null;
         category: import("@prisma/client").$Enums.EventCategory;
         description: string | null;
+        id: string;
         createdAt: Date;
         updatedAt: Date;
     }>;
